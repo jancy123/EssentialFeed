498638ac8c04ae9da955920d8698cd9190bb5e84
jancy123 <jancelots@gmail.com>
2023-03-04 19:32:38 -0800

    Create FeedItem Model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jancy on 3/4/23.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
