20023508126141d8182fb69dea2c974eedc6a159
jancy123 <jancelots@gmail.com>
2023-08-03 22:42:13 -0700

    Add save result type alias to protect code from potential breaking changes

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -74,7 +74,7 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [SaveResult]()
         sut?.save([uniqueItem()]) { receivedResults.append($0)}
         
         sut = nil
@@ -87,7 +87,7 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [SaveResult]()
         sut?.save([uniqueItem()]) { receivedResults.append($0)}
         
         store.completeDeletionSuccessfully()
